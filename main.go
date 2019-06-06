2ca96682a2b56b85584f01fc191d46fb2c023d19
dabai <yang_youwei81@163.com>
2019-06-06 14:48:31 +0800

    note

--- main.go
+++ main.go
@@ -13,13 +13,23 @@ import (
 	"strings"
 )
 
+//目标目录的父目录
 var Parents_dir string
-var Same_file_dir	string
-var source_path string
-var file_list []string
-var same_file_list []string
-//var file_md5 = make(map[string]string)
 
+//根据目标目录的路径，在目标目录的父目录下创建重复文件目录
+var Same_file_dir	string
+
+//指定要去重的目录
+var source_path string
+
+//遍历目标目录的所有文件，存到slice中
+var file_list []string
+
+//经过计算对比md5值，将重复文件目录放到slice中
+var same_file_list []string
+
+//根据参数初始化变量
+//父目录及重复文件目录创建
 func init()  {
 	s := flag.String("f","","-f C:\\Users\\yyw\\go\\src\\github.com\\yangyouwei\\quchong\\test 访问url")
 	flag.Parse()
@@ -41,13 +51,17 @@ func init()  {
 	}
 }
 
+
 func main()  {
+	//遍历目录
 	fn := getfilelist(source_path)
 	file_list = *fn
 
+	//比较md5值，重复的放到重复的slice中，不重复的放到map中
 	sn := diff_md5(file_list)
 	same_file_list = *sn
 
+	//读取重复文件的slice，移动文件到指定目录
 	move_file(same_file_list,Parents_dir,Same_file_dir)
 }
 
@@ -88,12 +102,15 @@ func diff_md5(fl []string) *[]string{
 	var s []string
 
 	for _,k := range fl {
+		//计算md5值
 		a := md5_sum(k)
 		if _, ok := f[a]; ok {
 			//fmt.Println("true")
+			//重复的吸入重复slice中
 			s = append(s,k)
 		}else{
 			//fmt.Println("false")
+			//不重复的加入map中
 			f[a] = k
 		}
 	}
@@ -117,6 +134,7 @@ func move_file(same_file []string, parents_dir string, same_file_path string) {
 		//需要在存放相同文件的目录里创建文件相应的层级目录
 		d_patch := same_file_path + string(sub_path)
 
+		//创建文件原来的目录层级结构
 		err := os.MkdirAll(d_patch, os.ModePerm)
 		if err != nil {
 			log.Println(err)
@@ -129,6 +147,7 @@ func move_file(same_file []string, parents_dir string, same_file_path string) {
 			log.Println(err)
 			break
 		}
+		//打印移动结果
 		fmt.Println(v + "move to " + d_patch)
 	}
 }
